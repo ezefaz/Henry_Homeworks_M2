85fba2715c5073ccab971a595a78df49d0a962cd
ezefaz <ezefaz@gmail.com>
2022-04-26 09:54:29 -0300

    Redux HW with Vanilla

--- 11-Redux/hw-js/src/index.js
+++ 11-Redux/hw-js/src/index.js
@@ -2,29 +2,39 @@ const { createStore } = require('redux');
 const contador = require('./reducer');
 const { incremento, decremento } = require('./actions');
 
+// en el caso de que tenga mas de un reducer:
+// let combined = combineReducer(r1, r2, r3 ...)
+// y luego al store le pasariamos createStore(combine)
+
+
 // En esta linea creamos nuestro store. Pasandole como parametro nuestro Reducer
-var store;
+
+var store = createStore(contador); // al store SIEMPRE le paso un UNICO reducer.
 
 // Obtenemos el elemento con el id `valor`.
-var valor;
+var valor = document.getElementById('valor');
 
 // Esta funcion nos va a servir para actualizar nuestro DOM con el valor que tengamos en nuestro Store.
 // En el primer render y cada vez que nos subscribamos al Store.
 // Utilizamos el elemento obtenido arriba para mostrar el State.
 function renderContador() {
   // Obtenemos la propiedad 'contador' de nuestro store:
-  
+  valor.innerText = store.getState().contador
   // Seteamos el numero obtenido como texto dentro del elemento con id 'valor':
 
 }
 
 // Ejecutamos la funcion 'renderContador':
-
+renderContador();
 
 
 // Nos subscribimos al store pasandole la misma funcion. Asi cada vez que llegue una accion, ejecutamos la funcion:
+store.subscribe(() => {console.log(store.getState())});
 
+store.subscribe(renderContador);
 
 
 // Por ultimo, utilizamos los botones de nuestro HTML para que cada vez que hagamos click,
 // hagan un dispatch al store de la accion correspondiente:
+document.getElementById('incremento').addEventListener = () => store.dispatch(incremento());
+document.getElementById('decremento').addEventListener = () => store.dispatch(decremento());
